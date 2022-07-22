09ab5d1ecd0c6bd668f7d99ee51178477c125f12
hathaipat-p <hathaipat44@hotmail.co.th>
2022-07-22 18:51:46 +0700

    update add-law page

--- src/app/components/add-law/add-law.component.ts
+++ src/app/components/add-law/add-law.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IMultiSelectOption, IMultiSelectSettings, IMultiSelectTexts } from 'ngx-bootstrap-multiselect';
 
 @Component({
   selector: 'app-add-law',
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddLawComponent implements OnInit {
 
-  constructor() { }
+  // Default selection
+  systemModel: number[] = [];
+  businessModel: number[] = [];
+
+  // Settings configuration
+  mySettings: IMultiSelectSettings = {
+    enableSearch: true,
+    checkedStyle: 'fontawesome',
+    buttonClasses: 'btn btn-default',
+    containerClasses: 'container p-3 my-3 border',
+    displayAllSelectedText: true,
+    showCheckAll:true,
+  };
+
+  // Text configuration
+  myTexts: IMultiSelectTexts = {
+    checkAll: 'Select all',
+    uncheckAll: 'Unselect all',
+    checked: 'item selected',
+    checkedPlural: 'items selected',
+    searchPlaceholder: 'Find',
+    searchEmptyResult: 'Nothing found...',
+    searchNoRenderText: 'Type in search box to see results...',
+    defaultTitle: 'Select',
+    allSelected: 'All selected',
+  };
+
+  systemOptions: IMultiSelectOption[] = [
+    { id: 1, name: 'CPF SHE & En MS'},
+    { id: 2, name: 'ISO 14001'},
+    { id: 3, name: 'OSHAS 18001'},
+    { id: 4, name: 'ISO50001'},
+  ];
+
+ businessOptions: IMultiSelectOption[] = [
+    { id: 1, name: 'Food'},
+    { id: 2, name: 'Farm'},
+    { id: 3, name: 'Feed'},
+    { id: 4, name: 'Trade'},
+    { id: 6, name: 'Other'}
+  ];
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
+
+  onChange() {
+    console.log(this.systemModel);
+}
 
 }
